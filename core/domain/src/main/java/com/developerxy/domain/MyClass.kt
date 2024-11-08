fb184c28eab5db0d506c29b9cbc30c1f26936ebd
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-11-08 13:20:42 +0100

    Create and connect app modules

---
+++ core/domain/src/main/java/com/developerxy/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.developerxy.domain
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Snoozeloo"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":app")
+include(":feature:youralarms")
+include(":feature:alarmsettings")
+include(":feature:ringtonesettings")
+include(":feature:alarmtrigger")
+include(":core:designsystem")
+include(":core:domain")
+include(":core:data")
+include(":core:database")
