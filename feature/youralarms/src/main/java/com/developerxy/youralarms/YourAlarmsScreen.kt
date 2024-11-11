5c6c302417a9fc2b6cfdeb2e324b10df1fa4ce46
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-11-11 17:01:41 +0100

    Display an empty view if there are no alarms available.

--- feature/youralarms/src/main/java/com/developerxy/youralarms/YourAlarmsScreen.kt
+++ feature/youralarms/src/main/java/com/developerxy/youralarms/YourAlarmsScreen.kt
@@ -1,23 +1,29 @@
 package com.developerxy.youralarms
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.developerxy.designsystem.icon.SnoozelooIcons
 import com.developerxy.youralarms.ui.AlarmsList
 import org.koin.androidx.compose.koinViewModel
 
@@ -55,8 +61,41 @@ fun YourAlarmsScreen(
                     )
             )
             Spacer(modifier = Modifier.height(24.dp))
-            AlarmsList(alarms = alarms)
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                if (alarms.isEmpty()) {
+                    NoAlarmsView()
+                } else {
+                    AlarmsList(alarms = alarms)
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun NoAlarmsView(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(horizontal = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = SnoozelooIcons.Alarm,
+            tint = MaterialTheme.colorScheme.primary,
+            contentDescription = null
+        )
+        Text(
+            "It's empty! Add the first alarm so you don't miss an important moment!",
+            modifier = Modifier.padding(top = 32.dp),
+            style = MaterialTheme.typography.labelMedium.copy(
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center
+            )
+        )
     }
 }
 
