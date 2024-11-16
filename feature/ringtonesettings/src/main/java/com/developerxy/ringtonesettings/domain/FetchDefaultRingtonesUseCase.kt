e4046fbbd16db85f63f367de0b0513e47bf14ead
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-11-16 13:33:59 +0100

    Add use case to fetch default alarm ringtones

---
+++ feature/ringtonesettings/src/main/java/com/developerxy/ringtonesettings/domain/FetchDefaultRingtonesUseCase.kt
@@ -0,0 +1,27 @@
+package com.developerxy.ringtonesettings.domain
+
+import android.content.Context
+import android.media.RingtoneManager
+import android.net.Uri
+import com.developerxy.ringtonesettings.domain.model.RingtoneInfo
+
+class FetchDefaultRingtonesUseCase(private val context: Context) {
+    operator fun invoke(): List<RingtoneInfo> {
+        val ringtoneManager = RingtoneManager(context).apply {
+            setType(RingtoneManager.TYPE_ALARM)
+        }
+
+        val ringtoneList = mutableListOf<RingtoneInfo>()
+        val cursor = ringtoneManager.cursor
+        while (cursor.moveToNext()) {
+            val title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)
+            val uriString = cursor.getString(RingtoneManager.URI_COLUMN_INDEX)
+            val id = cursor.getString(RingtoneManager.ID_COLUMN_INDEX)
+            val uri = Uri.parse("$uriString/$id")
+
+            ringtoneList.add(RingtoneInfo(title, uri))
+        }
+
+        return ringtoneList
+    }
+}
---
+++ feature/ringtonesettings/src/main/java/com/developerxy/ringtonesettings/domain/model/RingtoneInfo.kt
@@ -0,0 +1,5 @@
+package com.developerxy.ringtonesettings.domain.model
+
+import android.net.Uri
+
+data class RingtoneInfo(val title: String, val uri: Uri)
